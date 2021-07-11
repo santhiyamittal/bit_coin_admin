4a2a8d9d8ff46f73a54553cec8d8cf3715ff532c
bitcionia <85541189+bitcionia@users.noreply.github.com>
2021-07-11 13:07:00 +0530

    fix commit

--- src/app/components/pages/backendpage/backendpage.component.ts
+++ src/app/components/pages/backendpage/backendpage.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BackendpageComponent implements OnInit {
 
+  
+  public days;
+  public hours;
+  public minutes;
+  public seconds;
   constructor() { }
 
   ngOnInit(): void {
+    let countDown = new Date('Dec 1, 2022 00:00:00').getTime();
+    let time = setInterval(()=>{
+      let now = new Date().getTime();
+      let distance = countDown - now;
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / ( 1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / ( 1000 * 60 ));
+      this.seconds = Math.floor((distance % (1000 * 60 )) / 1000);
+
+      if(distance < 0){
+        clearInterval(time);
+      }
+    }, 1000)
   }
 
 }
